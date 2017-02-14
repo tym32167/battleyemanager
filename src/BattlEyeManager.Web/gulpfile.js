71a044c641af9cccd09867817ba7b4abe93864de
Artem Muradov <tym32167@gmail.com>
2017-02-15 00:41:44 +0100

    -

--- src/BattlEyeManager.Web/gulpfile.js
+++ src/BattlEyeManager.Web/gulpfile.js
@@ -8,16 +8,25 @@
 /// Define paths 
 var srcPaths = {
     app: ['Scripts/app/main.ts', 'Scripts/app/**/*.ts'],
-    js: ['Scripts/js/**/*.js']
+    js: ['Scripts/js/**/*.js',
+        'node_modules/core-js/client/shim.min.js',
+        'node_modules/zone.js/dist/zone.js',
+        'node_modules/reflect-metadata/Reflect.js',
+        'node_modules/systemjs/dist/system.src.js',
+        'node_modules/typescript/lib/typescript.js'],
+    js_angular: ['node_modules/@angular/**'],
+    js_rxjs: ['node_modules/rxjs/**']
 };
 
 var destPaths = {
     app: 'wwwroot/app/',
-    js: 'wwwroot/js/'
+    js: 'wwwroot/js/',
+    js_angular: 'wwwroot/js/@angular/',
+    js_rxjs: 'wwwroot/js/rxjs/'
 };
 
 // Compile, minify and create sourcemaps all TypeScript files and place them to wwwroot/app, together with their js.map files.
-gulp.task('app', function() {
+gulp.task('app', ['app_clean'], function () {
     return gulp.src(srcPaths.app)
         .pipe(gp_sourcemaps.init())
         .pipe(gp_typescript(require('./tsconfig.json').compilerOptions))
@@ -34,6 +43,10 @@ gulp.task('app_clean', function() {
 
 // Copy all JS files from external libraries to wwwroot/js 
 gulp.task('js', function () {     
+    gulp.src(srcPaths.js_angular)        
+        .pipe(gulp.dest(destPaths.js_angular));    
+    gulp.src(srcPaths.js_rxjs)        
+        .pipe(gulp.dest(destPaths.js_rxjs));     
     return gulp.src(srcPaths.js)         
     // .pipe(gp_uglify({ mangle: false })) // disable uglify         
     // .pipe(gp_concat('all-js.min.js')) // disable concat         
