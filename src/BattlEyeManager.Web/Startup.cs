f0f94f0ac3e9466c2d151debd79b3ef86ef65a80
Artem Muradov <tym32167@gmail.com>
2017-09-17 20:40:38 +0200

    -- empty core 2.0

--- src/BattlEyeManager.Web/Startup.cs
+++ src/BattlEyeManager.Web/Startup.cs
@@ -1,62 +1,29 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BattlEyeManager.Web
 {
     public class Startup
     {
-        private IHostingEnvironment _env;
-        private IConfigurationRoot _config;
-
-        public Startup(IHostingEnvironment env)
-        {
-            _env = env;
-
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(_env.ContentRootPath)
-                // .AddJsonFile("config.json")
-                .AddEnvironmentVariables();
-
-            _config = builder.Build();
-        }
-
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton(_config);
-
-            if (_env.IsEnvironment("Development") || _env.IsEnvironment("Testing"))
-            {
-                // services.AddScoped<IMailService, DebugMailService>();
-            }
-            else
-            {
-                // Implement a real Mail Service
-            }
-
-            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsEnvironment("Development"))
+            if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseStaticFiles();
-
-            app.UseMvc(config =>
+            app.Run(async (context) =>
             {
-                config.MapRoute(
-                    name: "Default",
-                    template: "{controller}/{action}/{id?}",
-                    defaults: new { controller = "App", action = "Index" }
-                );
+                await context.Response.WriteAsync("Hello World!");
             });
         }
     }
