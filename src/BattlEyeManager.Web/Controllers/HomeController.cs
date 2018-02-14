a2430e0c757efb2bce03b68cd748208edb97559f
Artem Muradov <tym32167@gmail.com>
2018-02-14 23:23:19 +0100

    basic mvc

---
+++ src/BattlEyeManager.Web/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BattlEyeManager.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
--- src/BattlEyeManager.Web/Startup.cs
+++ src/BattlEyeManager.Web/Startup.cs
@@ -1,69 +1,39 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace BattlEyeManager.Web
 {
     public class Startup
     {
-
-        public Startup(IHostingEnvironment env)
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
-        }
-
-        public IConfigurationRoot Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-
-            //loggerFactory.AddConsole();
-            loggerFactory.AddDebug();
-
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
             }
 
-            // Configure a rewrite rule to auto-lookup for standard default files such as index.html.    
-            app.UseDefaultFiles();
+            app.UseStaticFiles();
 
-            // Serve static files (html, css, js, images & more). See also the following URL: 
-            // https://docs.asp.net/en/latest/fundamentals/static-files.html for further reference. 
-
-            app.UseStaticFiles(new StaticFileOptions()
+            app.UseMvc(routes =>
             {
-                OnPrepareResponse = (context) =>
-                {
-                    // Disable caching for all static files.        
-                    context.Context.Response.Headers["Cache-Control"] = Configuration["StaticFiles:Headers:Cache-Control"];
-                    context.Context.Response.Headers["Pragma"] = Configuration["StaticFiles:Headers:Pragma"];
-                    context.Context.Response.Headers["Expires"] = Configuration["StaticFiles:Headers:Expires"];
-                }
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
-
-            // Add MVC to the pipeline     
-            app.UseMvc();
         }
     }
 }
