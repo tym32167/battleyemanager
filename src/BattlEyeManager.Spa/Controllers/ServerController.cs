4b781456a952f9f5e775b5b8bd17ba143ea62722
Artem Muradov <tym32167@gmail.com>
2018-02-28 21:37:03 +0100

    spa server list

---
+++ src/BattlEyeManager.Spa/Controllers/ServerController.cs
@@ -0,0 +1,35 @@
+﻿using BattlEyeManager.BE.Services;
+using BattlEyeManager.DataLayer.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BattlEyeManager.Spa.Controllers
+{
+    [Route("api/[controller]")]
+    public class ServerController : Controller
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public ServerController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        //[HttpGet("[action]")]
+        public Task<ServerInfo[]> Index()
+        {
+            return _appDbContext
+                .Servers
+                .OrderBy(x => x.Name)
+                .Select(x => new ServerInfo()
+                {
+                    Id = x.Id,
+                    Host = x.Host,
+                    Name = x.Name,
+                    Port = x.Port
+                }).ToArrayAsync();
+        }
+    }
+}
---
+++ src/BattlEyeManager.Spa/Startup.cs
@@ -0,0 +1,115 @@
+using BattlEyeManager.BE.Abstract;
+using BattlEyeManager.BE.Net;
+using BattlEyeManager.BE.ServerFactory;
+using BattlEyeManager.BE.Services;
+using BattlEyeManager.DataLayer.Context;
+using BattlEyeManager.DataLayer.Models;
+using BattlEyeManager.Spa.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.SpaServices.Webpack;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace BattlEyeManager.Spa
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+               options.UseMySql(@"server=localhost; database=battleyemanager; port=3306; user=root;"));
+
+            services.AddIdentity<ApplicationUser, ApplicationRole>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 8;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireLowercase = false;
+                options.Password.RequiredUniqueChars = 6;
+
+                // Lockout settings
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+                options.Lockout.MaxFailedAccessAttempts = 10;
+                options.Lockout.AllowedForNewUsers = true;
+
+                // User settings
+                options.User.RequireUniqueEmail = true;
+            });
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                // Cookie settings
+                options.Cookie.HttpOnly = true;
+                options.Cookie.Expiration = TimeSpan.FromDays(150);
+                options.LoginPath = "/Account/Login"; // If the LoginPath is not set here, ASP.NET Core will default to /Account/Login
+                options.LogoutPath = "/Account/Logout"; // If the LogoutPath is not set here, ASP.NET Core will default to /Account/Logout
+                options.AccessDeniedPath = "/Account/AccessDenied"; // If the AccessDeniedPath is not set here, ASP.NET Core will default to /Account/AccessDenied
+                options.SlidingExpiration = true;
+            });
+
+            //--------------------------------------------------------------------------
+
+            services.AddMvc();
+
+            services.AddSingleton<IIpService, IpService>();
+            services.AddSingleton<IBattlEyeServerFactory, WatcherBEServerFactory>();
+            services.AddSingleton<IBeServerAggregator, BeServerAggregator>();
+            services.AddSingleton<ServerStateService, ServerStateService>();
+            services.AddSingleton<DataRegistrator, DataRegistrator>();
+
+            services.AddSingleton<BELogic, BELogic>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
+                {
+                    HotModuleReplacement = true
+                });
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(
+                    name: "api",
+                    template: "api/{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapSpaFallbackRoute(
+                    name: "spa-fallback",
+                    defaults: new { controller = "Home", action = "Index" });
+            });
+        }
+    }
+}
