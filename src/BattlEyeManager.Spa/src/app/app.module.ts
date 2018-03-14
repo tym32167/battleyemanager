3817496d09de4431ca0df8e117903ef5085ef680
Artem Muradov <tym32167@gmail.com>
2018-03-14 23:44:35 +0100

    --

--- src/BattlEyeManager.Spa/src/app/app.module.ts
+++ src/BattlEyeManager.Spa/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
