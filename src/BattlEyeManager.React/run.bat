bcec45d9de919173198fc6a58ff2b33fcaa3c0ce
Artem Muradov <tym32167@gmail.com>
2018-05-22 23:42:39 +0200

    added jslint

--- src/BattlEyeManager.React/run.bat
+++ src/BattlEyeManager.React/run.bat
@@ -1,3 +1,9 @@
 run mysql note 
 D:\Tools\mariadb-10.2.13-winx64\bin\mysqld.exe
 dotnet watch run -p "D:\Code\bitbucket\battleyemanager\src\BattlEyeManager.Spa\BattlEyeManager.Spa.csproj"
+
+
+global packages
+
+npm install -g eslint
+eslint --init
