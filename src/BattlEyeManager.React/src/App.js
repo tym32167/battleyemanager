2a1e50d8f3ce4121ab179e02ca0e92101a5bd09f
Artem Muradov <tym32167@gmail.com>
2018-05-23 20:38:15 +0200

    bootstrap 4 + reactstrap

--- src/BattlEyeManager.React/src/App.js
+++ src/BattlEyeManager.React/src/App.js
@@ -1,10 +1,8 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
-
 import { Home } from './pages/home'
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
