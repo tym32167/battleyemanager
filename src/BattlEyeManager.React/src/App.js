ceccc2e42cd76ba1d70544d00749dc1daeb2524a
Artem Muradov <tym32167@gmail.com>
2018-06-03 22:51:56 +0200

    -

--- src/BattlEyeManager.React/src/App.js
+++ src/BattlEyeManager.React/src/App.js
@@ -1,24 +1,35 @@
 import { Home } from './pages/home'
 import { Users } from './pages/users'
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Test } from './pages/test';
 import { Login } from './pages/login/login';
 
-import store from './store';
 
-const  App = ()=><div className="App">
-<Router>
-  <div>
-    <Route exact path="/" component={Home} />
-    <Route exact path="/users" component={Users} />
-    <Route exact path="/test" component={Test} />
-    <Route exact path="/login" component={Login} />
-  </div>
-</Router>        
+
+const App = () => <div className="App">
+  <Router>
+    <div>
+      <PrivateRoute authed={false} exact path="/" component={Home} />
+      <PrivateRoute authed={false} exact path="/users" component={Users} />      
+      <PrivateRoute authed={false} exact path="/test" component={Test} />
+      <Route exact path="/login" component={Login} />
+    </div>
+  </Router>
 </div>;
 
 export default App;
 
+
+function PrivateRoute({ component: Component, authed, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={(props) => authed === true
+        ? <Component {...props} />
+        : <Redirect to={{ pathname: '/login', state: { from: props.location } }} />}
+    />
+  )
+}
--- src/BattlEyeManager.React/src/index.js
+++ src/BattlEyeManager.React/src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import store from './store';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
 registerServiceWorker();
