6230e261b2ee7c40550e2c937f50475050eeeb73
Artem Muradov <tym32167@gmail.com>
2018-07-17 22:49:30 +0200

    layout fix

--- src/BattlEyeManager.React/src/App.jsx
+++ src/BattlEyeManager.React/src/App.jsx
@@ -3,37 +3,34 @@ import { Router, Route, Redirect } from "react-router-dom";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Test, UserList, Login, Home } from './pages';
-import {history}  from './services/history';
-import {PageTemplate} from './shared_components';
+import { history } from './services/history';
+import { PageTemplate } from './shared_components';
 
-    const App = () => <div className="App">
-      <Router history={history}>
-        <div>
-          <DefaultLayout exact path="/" component={Home} />
-          <DefaultLayout exact path="/users" component={UserList} />
-          <DefaultLayout exact path="/test" component={Test} />
 
-          <Route exact path="/login" component={Login} />
-        </div>
-      </Router>
-    </div>;
+const App = () => <div className="App">
+  <Router history={history}>
+    <div>
+      <DefaultLayout exact path="/" component={Home} />
+      <DefaultLayout exact path="/users" component={UserList} />
+      <DefaultLayout exact path="/test" component={Test} />
+      <Route exact path="/login" component={Login} />
+    </div>
+  </Router>
+</div>;
 
 export default App;
 
-const DefaultLayout = ({component: Component, ...rest}) => {
+const DefaultLayout = ({ component: Component, ...rest }) => {
   return (
     <Route {...rest} render={matchProps => (
-      <PageTemplate>
+
+      localStorage.getItem('user')
+        ?
+        <PageTemplate>
           <Component {...matchProps} />
-      </PageTemplate>
+        </PageTemplate>
+        : 
+        <Redirect to={{ pathname: '/login', state: { from: matchProps.location } }} />
     )} />
   )
 };
-
-const PrivateRoute = ({ component: Component, ...rest }) => (
-  <Route {...rest} render={props => (
-    localStorage.getItem('user')
-      ? <Component {...props} />
-      : <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
-  )} />
-)
