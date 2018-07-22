4bd1a46b25db3826635f347196e5d74e6157eb0f
Artem Muradov <tym32167@gmail.com>
2018-07-22 14:57:34 +0200

    mysql

--- src/BattlEyeManager.React/run.bat
+++ src/BattlEyeManager.React/run.bat
@@ -7,10 +7,5 @@ global packages
 npm install -g eslint
 eslint --init
 
-run mysql as service
-mysql_install_db.exe --datadir=D:\data\mysql --service=MariaDB
-
-run mysql note 
-D:\Tools\mariadb-10.2.13-winx64\bin\mysqld.exe
 
 
