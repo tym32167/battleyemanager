6cafd3f0647f2bc025bd9ca84e02d2cdf7a8486d
Artem Muradov <tym32167@gmail.com>
2018-09-01 23:22:36 +0200

    cmon actions

--- src/BattlEyeManager.React/src/store/actions/commonActions.ts
+++ src/BattlEyeManager.React/src/store/actions/commonActions.ts
@@ -1,109 +1,106 @@
+import { Dispatch } from "redux";
 import { IIdentity } from "src/models/iidentity";
 import { IReadonlyService, IService } from "../../services";
 import { combineConstants, commonConstants } from "../constants";
 
-export const commonActions = {
-    addItem,
-    deleteItem,
-    getItem,
-    getItems,
-    updateItem,
-};
 
-function deleteItem<T extends IIdentity>(element: T, subject: string, service: IService<T>, successAction: any) {
-    return (dispatch: any) => {
-        dispatch(request(element));
-        service.deleteItem(element.id)
-            .then(
-                (item: any) => {
-                    dispatch(success(item));
-                    if (successAction) { successAction(item, dispatch); }
-                },
-                (error: any) => {
-                    dispatch(failure(error));
-                }
-            );
-    };
-    function request(item: any) { return { type: combineConstants(subject, commonConstants.DELETE_ITEM, commonConstants.ASYNC_REQUEST), item } }
-    function success(item: any) { return { type: combineConstants(subject, commonConstants.DELETE_ITEM, commonConstants.ASYNC_REQUEST_SUCCESS), item } }
-    function failure(error: any) { return { type: combineConstants(subject, commonConstants.DELETE_ITEM, commonConstants.ASYNC_REQUEST_FAILURE), error } }
+function requestAction<T>(item: T, subject: string, action: commonConstants) {
+    return { type: combineConstants(subject, action, commonConstants.ASYNC_REQUEST), item };
 }
 
-function addItem<T extends IIdentity>(element: T, subject: string, service: IService<T>, successAction: any) {
-    return (dispatch: any) => {
-        dispatch(request(element));
-        service.addItem(element)
-            .then(
-                (item: T) => {
-                    dispatch(success(item));
-                    if (successAction) { successAction(item, dispatch); }
-                },
-                (error: any) => {
-                    dispatch(failure(error));
-                }
-            );
-    };
-    function request(item: any) { return { type: combineConstants(subject, commonConstants.CREATE_ITEM, commonConstants.ASYNC_REQUEST), item } }
-    function success(item: any) { return { type: combineConstants(subject, commonConstants.CREATE_ITEM, commonConstants.ASYNC_REQUEST_SUCCESS), item } }
-    function failure(error: any) { return { type: combineConstants(subject, commonConstants.CREATE_ITEM, commonConstants.ASYNC_REQUEST_FAILURE), error } }
+function successAction<T>(item: T, subject: string, action: commonConstants) {
+    return { type: combineConstants(subject, action, commonConstants.ASYNC_REQUEST_SUCCESS), item };
 }
 
-function updateItem<T extends IIdentity>(element: T, subject: string, service: IService<T>, successAction: any) {
-    return (dispatch: any) => {
-        dispatch(request(element));
-        service.updateItem(element)
-            .then(
-                (item: any) => {
-                    dispatch(success(item));
-                    if (successAction) { successAction(item, dispatch); }
-                },
-                (error: any) => {
-                    dispatch(failure(error));
-                }
-            );
-    };
-    function request(item: any) { return { type: combineConstants(subject, commonConstants.UPDATE_ITEM, commonConstants.ASYNC_REQUEST), item } }
-    function success(item: any) { return { type: combineConstants(subject, commonConstants.UPDATE_ITEM, commonConstants.ASYNC_REQUEST_SUCCESS), item } }
-    function failure(error: any) { return { type: combineConstants(subject, commonConstants.UPDATE_ITEM, commonConstants.ASYNC_REQUEST_FAILURE), error } }
+function failureAction(error: any, subject: string, action: commonConstants) {
+    return { type: combineConstants(subject, action, commonConstants.ASYNC_REQUEST_FAILURE), error };
 }
 
-function getItems<T>(subject: string, service: IReadonlyService<T>) {
-    return (dispatch: any) => {
-        dispatch(request([]));
-        getPromise(service, (s: any) => s.getItems())
-            .then(
-                (items: any) => {
-                    dispatch(success(items));
-                },
-                (error: any) => {
-                    dispatch(failure(error));
-                }
-            );
-    };
-    function request(items: any) { return { type: combineConstants(subject, commonConstants.GET_ITEMS, commonConstants.ASYNC_REQUEST), items } }
-    function success(items: any) { return { type: combineConstants(subject, commonConstants.GET_ITEMS, commonConstants.ASYNC_REQUEST_SUCCESS), items } }
-    function failure(error: any) { return { type: combineConstants(subject, commonConstants.GET_ITEMS, commonConstants.ASYNC_REQUEST_FAILURE), error } }
+
+class CommonActions {
+    public deleteItem<T extends IIdentity>(element: T, subject: string, service: IService<T>, callback: any) {
+        return (dispatch: Dispatch<T>) => {
+            dispatch(requestAction(element, subject, commonConstants.DELETE_ITEM));
+            service.deleteItem(element.id)
+                .then(
+                    (item: T) => {
+                        dispatch(callback(item, subject, commonConstants.DELETE_ITEM));
+                        if (callback) { callback(item, dispatch); }
+                    },
+                    (error: any) => {
+                        dispatch(failureAction(error, subject, commonConstants.DELETE_ITEM));
+                    }
+                );
+        };
+    }
+
+    public addItem<T extends IIdentity>(element: T, subject: string, service: IService<T>, callback: any) {
+        return (dispatch: Dispatch<T>) => {
+            dispatch(requestAction(element, subject, commonConstants.CREATE_ITEM));
+            service.addItem(element)
+                .then(
+                    (item: T) => {
+                        dispatch(callback(item, subject, commonConstants.CREATE_ITEM));
+                        if (callback) { callback(item, dispatch); }
+                    },
+                    (error: any) => {
+                        dispatch(failureAction(error, subject, commonConstants.CREATE_ITEM));
+                    }
+                );
+        };
+    }
+
+    public updateItem<T extends IIdentity>(element: T, subject: string, service: IService<T>, callback: any) {
+        return (dispatch: Dispatch<T>) => {
+            dispatch(requestAction(element, subject, commonConstants.UPDATE_ITEM));
+            service.updateItem(element)
+                .then(
+                    (item: T) => {
+                        dispatch(callback(item, subject, commonConstants.UPDATE_ITEM));
+                        if (callback) { callback(item, dispatch); }
+                    },
+                    (error: any) => {
+                        dispatch(failureAction(error, subject, commonConstants.UPDATE_ITEM));
+                    }
+                );
+        };
+    }
+
+    public getItems<T>(subject: string, service: IReadonlyService<T>) {
+        return (dispatch: Dispatch<T>) => {
+            dispatch(requestAction([], subject, commonConstants.GET_ITEMS));
+            this.getPromise(service, (s: any) => s.getItems())
+                .then(
+                    (items: T[]) => {
+                        dispatch(successAction(items, subject, commonConstants.GET_ITEMS));
+                    },
+                    (error: any) => {
+                        dispatch(failureAction(error, subject, commonConstants.GET_ITEMS));
+                    }
+                );
+        };
+    }
+
+    public getItem<T>(id: string | number, subject: string, service: IReadonlyService<T>) {
+        return (dispatch: Dispatch<T>) => {
+            dispatch(requestAction({ id }, subject, commonConstants.GET_ITEM));
+            service.getItem(id)
+                .then(
+                    (item: T) => {
+                        dispatch(successAction(item, subject, commonConstants.GET_ITEM));
+                    },
+                    (error: any) => {
+                        dispatch(failureAction(error, subject, commonConstants.GET_ITEM));
+                    }
+                );
+        };
+    }
+
+    private getPromise(subject: any, invocator: any) {
+        if (typeof (subject) === 'function') { return subject(); }
+        return invocator(subject);
+    }
+
 }
 
-function getItem<T>(id: string | number, subject: string, service: IReadonlyService<T>) {
-    return (dispatch: any) => {
-        dispatch(request({ id }));
-        service.getItem(id)
-            .then(
-                (user: any) => {
-                    dispatch(success(user));
-                },
-                (error: any) => {
-                    dispatch(failure(error));
-                }
-            );
-    };
-    function request(item: any) { return { type: combineConstants(subject, commonConstants.GET_ITEM, commonConstants.ASYNC_REQUEST), item } }
-    function success(item: any) { return { type: combineConstants(subject, commonConstants.GET_ITEM, commonConstants.ASYNC_REQUEST_SUCCESS), item } }
-    function failure(error: any) { return { type: combineConstants(subject, commonConstants.GET_ITEM, commonConstants.ASYNC_REQUEST_FAILURE), error } }
-}
-
-function getPromise(subject: any, invocator: any) {
-    if (typeof (subject) === 'function') { return subject(); }
-    return invocator(subject);
-}
+export const commonActions = new CommonActions();
