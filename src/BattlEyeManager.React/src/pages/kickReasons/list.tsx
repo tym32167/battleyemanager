285ad4567568bd5db2fcb253565f648a2e80ae92
Artem Muradov <tym32167@gmail.com>
2018-10-29 23:54:41 +0100

    -

--- src/BattlEyeManager.React/src/pages/kickReasons/list.tsx
+++ src/BattlEyeManager.React/src/pages/kickReasons/list.tsx
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
+import { IKickReason } from 'src/models';
 
 interface IKickListProps {
+    data: IKickReason[]
 }
 
-class List extends Component<IKickListProps>{
+export class List extends Component<IKickListProps>{
 
+    constructor(props: IKickListProps) {
+        super(props);
+    }
+    public render() {
+        return (<React.Fragment>
+            <h1>List</h1>
+        </React.Fragment>)
+    }
 }
