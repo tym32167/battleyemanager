c8e5223f7b0f87a42a9e606b28aaf05abe291bdb
Artem Muradov <tym32167@gmail.com>
2018-12-08 00:19:39 +0100

    nlog

--- src/BattlEyeManager.Spa/Program.cs
+++ src/BattlEyeManager.Spa/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
+using System;
 
 namespace BattlEyeManager.Spa
 {
@@ -7,13 +10,37 @@ namespace BattlEyeManager.Spa
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+
+            // NLog: setup the logger first to catch all errors
+            var logger = NLog.Web.NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            try
+            {
+                logger.Debug("init main");
+                BuildWebHost(args).Run();
+            }
+            catch (Exception ex)
+            {
+                //NLog: catch setup errors
+                logger.Error(ex, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                NLog.LogManager.Shutdown();
+            }
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
+        private static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseUrls("http://localhost:58175", "http://192.168.0.10:58175")
+                .UseUrls("http://localhost:58175"/*, "http://192.168.0.10:58175"*/)
                 .UseStartup<Startup>()
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(LogLevel.Trace);
+                })
+                .UseNLog()  // NLog: setup NLog for Dependency injection
                 .Build();
     }
 }
