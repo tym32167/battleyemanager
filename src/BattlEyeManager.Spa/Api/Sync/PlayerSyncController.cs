5aa65db0f459bf2dd264849048ae06d8ef1340d6
Artem Muradov <tym32167@gmail.com>
2020-04-04 09:05:35 -0400

    #30 back to client

---
+++ src/BattlEyeManager.Spa/Api/Sync/PlayerSyncController.cs
@@ -0,0 +1,58 @@
+﻿using BattlEyeManager.Spa.Core;
+using BattlEyeManager.Spa.Infrastructure.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace BattlEyeManager.Spa.Api.Sync
+{
+    [Produces("application/json")]
+    [ApiController]
+    [Authorize]
+    public class PlayerSyncController : BaseController
+    {
+        private readonly PlayerSyncService _playerSyncService;
+
+        public PlayerSyncController(PlayerSyncService playerSyncService)
+        {
+            _playerSyncService = playerSyncService;
+        }
+
+        [HttpGet("api/sync/players")]
+        public async Task<IActionResult> Get(int offset, int count)
+        {
+            if (offset < 0) return BadRequest(nameof(offset));
+            if (count < 0 || count > 1000) BadRequest(nameof(count));
+            var players = await _playerSyncService.GetPlayers(offset, count);
+            var cnt = await _playerSyncService.GetPlayersCount();
+
+            var resp = new PlayerSyncResponse()
+            {
+                Count = cnt, 
+                Players = players
+            };
+
+
+
+            return Ok(resp);
+        }
+
+    }
+
+    public class PlayerSyncResponse
+    {
+        public int Count { get; set; }
+        public PlayerSyncDto[] Players { get; set; }
+    }
+
+    public class PlayerSyncDto
+    {
+        public string SteamId { get; set; }
+        public string GUID { get; set; }
+        public string Comment { get; set; }
+        public string Name { get; set; }
+        public string IP { get; set; }
+        public DateTime LastSeen { get; set; }
+    }
+}
---
+++ src/BattlEyeManager.Spa/Infrastructure/Services/PlayerSyncService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using BattlEyeManager.DataLayer.Context;
+using BattlEyeManager.Spa.Api.Sync;
+using BattlEyeManager.Spa.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BattlEyeManager.Spa.Infrastructure.Services
+{
+    public class PlayerSyncService
+    {
+        private readonly IServiceScopeFactory _scopeFactory;
+
+        public PlayerSyncService(IServiceScopeFactory scopeFactory)
+        {
+            _scopeFactory = scopeFactory;
+        }
+
+        public async Task<int> GetPlayersCount()
+        {
+            using (var scope = _scopeFactory.CreateScope())
+            {
+                using (var ctx = scope.ServiceProvider.GetService<AppDbContext>())
+                {
+                    return await ctx.Players.CountAsync();
+                }
+            }
+        }
+
+        public async Task<PlayerSyncDto[]> GetPlayers(int offset, int count)
+        {
+            using (var scope = _scopeFactory.CreateScope())
+            {
+                using (var ctx = scope.ServiceProvider.GetService<AppDbContext>())
+                {
+                    var dbItems = await ctx.Players
+                        
+                        .OrderBy(x => x.Id)
+                        .Skip(offset)
+                        .Take(count)
+                        .ToArrayAsync();
+
+                    var items = dbItems
+                        .Select(x => Mapper.Map<PlayerSyncDto>(x))
+                        .ToArray();
+
+                    return items;
+                }
+            }
+        }
+    }
+}
