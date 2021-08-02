6cb5f3c218b8407dd56f85fb3c1bf015f3ec3b26
Artem Muradov <tym32167@gmail.com>
2021-08-01 23:54:14 -0400

    unsecure swagger

--- src/BattlEyeManager.Spa/Startup.cs
+++ src/BattlEyeManager.Spa/Startup.cs
@@ -277,21 +277,19 @@ namespace BattlEyeManager.Spa
         {
             store.Database.Migrate();
 
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Battleye Manager API V1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-
-
-                // Enable middleware to serve generated Swagger as a JSON endpoint.
-                app.UseSwagger();
-
-                // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
-                // specifying the Swagger JSON endpoint.
-                app.UseSwaggerUI(c =>
-                {
-                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Battleye Manager API V1");
-                });
-
             }
             else
             {
