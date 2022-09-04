287414225681f6ba516aad1b04ff2db1fb6b5981
Artem Muradov <tym32167@gmail.com>
2022-09-04 11:05:01 -0400

    ip util

---
+++ src/BattlEyeManager.Spa/Api/UtilController.cs
@@ -0,0 +1,31 @@
+﻿using BattlEyeManager.Core;
+using BattlEyeManager.Spa.Core;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BattlEyeManager.Spa.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    // public class UtilController : BaseController
+    public class UtilController : Controller
+    {
+        private readonly IIpService ipService;
+
+        public UtilController(IIpService service)
+        {
+            this.ipService = service;
+        }
+
+        [HttpGet("ip")]
+        public async Task<IActionResult> CheckIP()
+        {
+            var remoteIpAddress = this.Request.HttpContext.Connection.RemoteIpAddress;
+
+            var country = ipService.GetCountry(remoteIpAddress.ToString());
+
+            return Ok(new { IP = remoteIpAddress.ToString(), country = country });
+        }
+    }
+}
